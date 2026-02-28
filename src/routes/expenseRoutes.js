5e216551eda2cf9292e454759f99891229785184
imbozen <imbozencedarcrest.edu>
2026-02-28 00:23:40 -0500

    Add expense routes

---
+++ src/routes/expenseRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const expenseController = require('../controllers/expenseController');
+
+router.post('/expenses', expenseController.createExpense);
+router.get('/expenses', expenseController.fetchExpenses);
+router.get('/expenses/total', expenseController.fetchTotal);
+
+module.exports = router;

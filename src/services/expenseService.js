dc11e18c7a116d1e3b195575063aeb36ab3d649e
imbozen <imbozencedarcrest.edu>
2026-02-28 00:14:53 -0500

    Add expense service with validation and total calculation

---
+++ src/services/expenseService.js
@@ -0,0 +1,32 @@
+const Expense = require('../models/expense');
+
+const expenses = [];
+
+function addExpense(description, amount) {
+  if (!description) {
+    throw new Error("Description is required");
+  }
+
+  if (typeof amount !== "number" || amount <= 0) {
+    throw new Error("Amount must be a positive number");
+  }
+
+  const expense = new Expense(description, amount);
+  expenses.push(expense);
+
+  return expense;
+}
+
+function getAllExpenses() {
+  return expenses;
+}
+
+function getTotalExpenses() {
+  return expenses.reduce((total, expense) => total + expense.amount, 0);
+}
+
+module.exports = {
+  addExpense,
+  getAllExpenses,
+  getTotalExpenses
+};
